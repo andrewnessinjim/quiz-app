32f19011d26b88e8c20b2fdca33725fb0cba1757
Andrew Nessin <andrewnessinjim@yahoo.com>
2023-12-08 06:27:46 +0000

    Improve theme toggle UX

--- src/components/ThemeToggle/ThemeToggle.js
+++ src/components/ThemeToggle/ThemeToggle.js
@@ -1,26 +1,69 @@
-import PropTypes from "prop-types";
 import React from "react";
-import { styled } from "styled-components";
+import PropTypes from "prop-types";
+import { css, styled } from "styled-components";
+
+import * as Toggle from "@radix-ui/react-toggle";
+import { SunIcon, MoonIcon } from "@radix-ui/react-icons";
 
 import { AppThemeContext } from "../AppThemeProvider";
 
-const Wrapper = styled.div``;
-const StLabel = styled.label`
-  color: ${(p) => p.theme.colors.plum12};
+const StToggleRoot = styled(Toggle.Root)`
+  width: 84px;
+  border-radius: 32px;
+  border: none;
+  padding: 4px;
+  background-color: ${(p) => p.theme.colors.mauve4};
+  position: relative;
+  margin-bottom: 32px;
 `;
+
+const IconsContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 6px;
+`;
+
+const iconStyles = css`
+  stroke: ${(p) => p.theme.colors.plum10};
+  stroke-width: 1px;
+  height: 18px;
+  width: 18px;
+`;
+const StSunIcon = styled(SunIcon)`
+  ${iconStyles};
+`;
+
+const StMoonIcon = styled(MoonIcon)`
+  ${iconStyles};
+`;
+
+const StThemeIndicator = styled.div`
+  height: 30px;
+  width: 30px;
+  border-radius: 50%;
+  background: ${(p) => p.theme.colors.plum7};
+  position: absolute;
+  top: 4px;
+  left: 4px;
+  transition: 150ms transform ease-in;
+  opacity: 0.8;
+
+  ${StToggleRoot}[data-state="on"] & {
+    transform: translateX(46px);
+  }
+`;
+
 function ThemeToggle() {
   const { isDarkTheme, setIsDarkTheme } = React.useContext(AppThemeContext);
 
   return (
-    <Wrapper>
-      <input
-        type="checkbox"
-        id="checkbox-theme"
-        checked={isDarkTheme}
-        onChange={(e) => setIsDarkTheme(e.target.checked)}
-      />
-      <StLabel for="checkbox-theme">Dark Theme</StLabel>
-    </Wrapper>
+    <StToggleRoot pressed={isDarkTheme} onPressedChange={setIsDarkTheme}>
+      <IconsContainer>
+        <StSunIcon />
+        <StMoonIcon />
+      </IconsContainer>
+      <StThemeIndicator />
+    </StToggleRoot>
   );
 }
 
